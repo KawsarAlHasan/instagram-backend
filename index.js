2988a53d120620e9058b6fffbc37cb4398697731
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-12-29 02:42:52 +0600

    get and post method add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { ServerApiVersion, MongoClient } = require('mongodb')
+const { ServerApiVersion, MongoClient, ObjectId } = require('mongodb')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 4000
@@ -18,11 +18,55 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect()
-    const studentCollection = client.db('lovepost').collection('users')
+    const usersCollection = client.db('lovepost').collection('users')
+    const imagePostCollection = client.db('lovepost').collection('imagePost')
+
+    // get users
+    app.get('/users', async (req, res) => {
+      const query = {}
+      const cursor = usersCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    // get one user
+    app.get('/user/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await usersCollection.findOne(query)
+      res.send(result)
+    })
 
     // create users
-    app.put('/user', (req, res) => {
-      res.send('Got a PUT request at /user')
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email
+      const userInfo = req.body
+      console.log(userInfo)
+      const filter = { email: email }
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          displayName: userInfo.displayName,
+          photoURL: userInfo.photoURL,
+        },
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc, options)
+      res.send(result)
+    })
+
+    // get images
+    app.get('/post/images', async (req, res) => {
+      const query = {}
+      const cursor = imagePostCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    // image post
+    app.post('/post/image', async (req, res) => {
+      const newPost = req.body
+      const result = await imagePostCollection.insertOne(newPost)
+      res.send(result)
     })
   } finally {
   }
