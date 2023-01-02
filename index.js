cd6ba76af9aa2b91e9573fb604468bbec091657f
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2023-01-03 03:09:24 +0600

    rander github chack

--- index.js
+++ index.js
@@ -77,6 +77,22 @@ async function run() {
       const result = await cursor.toArray()
       res.send(result)
     })
+
+    // get one image post
+    app.get('/post/image/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await imagePostCollection.findOne(query)
+      res.send(result)
+    })
+
+    // imagePost Delete
+    app.delete('/post/image/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await imagePostCollection.deleteOne(query)
+      res.send(result)
+    })
   } finally {
   }
 }
